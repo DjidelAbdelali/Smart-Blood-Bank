4399bbfd6dd61a7a25f0230ff8444580dce8a09d
DjidelAbdelali <djidelabdelali@gmail.com>
2025-02-21 23:52:02 +0100

    Delete src directory

--- src/app/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <main className="p-10">
-      <h1 className="text-3xl font-bold">About Us</h1>
-      <p className="text-lg mt-4">We are a platform dedicated to connecting blood donors with those in need.</p>
-    </main>
-  );
-}
